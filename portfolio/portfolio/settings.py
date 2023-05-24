5cba0ff92d6f006e32bbbb45067e4cffcbbdbf61
Dennismwendwa <dennismusemb2@.com>
2023-05-24 06:55:06 +0300

    removed link

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 CSRF_TRUSTED_ORIGINS = ["https://dennissoftware.up.railway.app"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
